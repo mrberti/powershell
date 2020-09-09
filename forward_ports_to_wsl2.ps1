a4e5890a4cf547cdec5c71e335428b5458015381
Simon Bertling <berti.x@gmx.de>
2020-09-09 17:14:51 +0200

    now using powershell only commands. makes it quite nasty...

--- forward_ports_to_wsl2.ps1
+++ forward_ports_to_wsl2.ps1
@@ -6,33 +6,53 @@
 # Based on: https://github.com/microsoft/WSL/issues/4150#issuecomment-504209723
 $ports=@(5000,8080);
 $ip_local = "0.0.0.0";
+$ip_wsl = "";
 
-# Get the local ip address of WSL 2
-$ip_wsl = wsl -- /bin/sh -c "ifconfig | grep -o 'inet [^ ]*' | grep -v 127 | sed 's/inet //g'";
+# Get the local ip address of WSL 2. It seems, that the return value of `wsl`
+# is an array of lines. Unfortunately, I did not find a way to parse this array
+# with regular expressions without iterating.
+$ifconfig = wsl ifconfig;
+foreach ($l in $ifconfig) {
+    $m = $l -match "inet (?<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})";
+    if($m) {
+        $ip_found = $Matches.ip;
+        if($ip_found -notmatch "^127") {
+            $ip_wsl = $ip_found;
+            Write-Output "Found WSL IP '$ip_wsl'";
+            break;
+        }
+    }
+}
+
 if( -Not $ip_wsl ){
-    Write-Output "The Script Exited, the ip address of WSL 2 cannot be found";
+    Write-Output "ERROR: The ip address of WSL 2 cannot be found";
     exit;
 }
 
 $ports_array = $ports -join ",";
-Write-Output $ports_array;
+Write-Output "Forwarding Ports '$ports_array' from '$ip_local' to '$ip_wsl'";
 
 function RunAsAdmin($commands) {
-    # This function runs all $commands as administrator
-    # Expecting an array as $commands
-    $snippet=$commands -join " ; ";
-    $snippet= "cd '$pwd'; Invoke-Expression '" + $snippet + "' ; read-host Press ENTER to continue... ;";
+    # This function runs all $commands as administrator. Expecting an array as
+    # $commands.
+    $snippet = $commands -join " ; ";
+    $snippet = "cd '$pwd'; Invoke-Expression '" + $snippet + "' ; "
     if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $snippet += "read-host Press ENTER to continue... ;"
         Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command $snippet";
+    } else {
+        Invoke-Expression $snippet;
     }
 }
 
 $commands=@();
-#adding Exception Rules for inbound and outbound Rules
-# For escaping `"` characters, see: https://stackoverflow.com/questions/6714165/powershell-stripping-double-quotes-from-command-line-arguments
-$commands +=  "Remove-NetFireWallRule -DisplayName \""WSL 2 \""";
-$commands +=  "New-NetFireWallRule -DisplayName \""WSL 2\"" -Direction Outbound -LocalPort $ports_array -Action Allow -Protocol TCP -Description \""Created by script - Simon\""";
-$commands +=  "New-NetFireWallRule -DisplayName \""WSL 2\"" -Direction Inbound -LocalPort $ports_array -Action Allow -Protocol TCP -Description \""Created by script - Simon\""";
+# Adding Exception Rules for inbound and outbound Rules
+# For escaping `"` characters, see:
+# https://stackoverflow.com/questions/6714165/powershell-stripping-double-quotes-from-command-line-arguments
+$date = Get-Date;
+$commands +=  "Remove-NetFireWallRule -DisplayName \""WSL 2\""";
+$commands +=  "New-NetFireWallRule -DisplayName \""WSL 2\"" -Direction Outbound -LocalPort $ports_array -Action Allow -Protocol TCP -Description \""Created by script - $date\""";
+$commands +=  "New-NetFireWallRule -DisplayName \""WSL 2\"" -Direction Inbound -LocalPort $ports_array -Action Allow -Protocol TCP -Description \""Created by script - $date\""";
 
 foreach($port in $ports) {
     # Adding port proxy
@@ -40,4 +60,8 @@ foreach($port in $ports) {
     $commands += "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$ip_local connectport=$port connectaddress=$ip_wsl";
 }
 
+Write-Output "Running the following commands as Admin now:`n"
+$commands
+Write-Output "`nPress any key to continue or ctrl+c to abort..."
+$null = $Host.UI.RawUI.ReadKey('NoEcho, IncludeKeyDown');
 RunAsAdmin($commands)
